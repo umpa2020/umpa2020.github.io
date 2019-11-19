d3992d18cc0e7695952957826611dfda9d3c2de7
YunkwonShin <syg4806@gmail.com>
2019-11-19 09:17:35 +0900

    Revert "Merge branch 'master' of http://gitlab.mintech.kr/jsceno/jsceno-android"
    This reverts commit b42e4907aaa5ef4d6a725c9c0d27bd931a4d574e, reversing
    changes made to 2d5bdb35d556b0ad72486a1e30e0653d9c92e48f.

--- app/src/main/java/com/superdroid/facemaker/Activity/LoadActivity.kt
+++ app/src/main/java/com/superdroid/facemaker/Activity/LoadActivity.kt
@@ -2,6 +2,7 @@ package com.superdroid.facemaker.Activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.superdroid.facemaker.R
