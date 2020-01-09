3deffbfe3cea1ca7445c01029854d9b58b0517f4
jungsoojin <jdyun53658315@gmail.com>
2020-01-10 02:00:54 +0900

    새로운 액티비티 추가(profile) / fragment - user_map, fragment_user_community추가

---
+++ RunShare/app/src/main/java/com/korea50k/RunShare/Activities/Profile/FragmentUserRace.kt
@@ -0,0 +1,24 @@
+package com.korea50k.RunShare
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FragmentUserRace : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_race, container, false)
+    }
+
+
+}
