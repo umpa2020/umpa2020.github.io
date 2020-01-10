a1849cea98bdf5e6b45b64688f45d91a1924916e
jungsoojin <jdyun53658315@gmail.com>
2020-01-10 21:56:19 +0900

    setting, profile 관련 액티비티 만듬, onclick 메소드 구현

---
+++ RunShare/app/src/main/java/com/korea50k/RunShare/Activities/Profile/MyInformationActivity.kt
@@ -0,0 +1,13 @@
+package com.korea50k.RunShare.Activities.Profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.korea50k.RunShare.R
+
+class MyInformationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_information)
+    }
+}
