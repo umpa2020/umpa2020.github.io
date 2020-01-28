0edb37396752b2a19ac8e7d4a87a67ed0dacd2f9
jungsoojin <jdyun53658315@gmail.com>
2020-01-28 09:01:01 +0900

    bottom navigation 수정

--- Tracer/app/src/main/java/com/korea50k/tracer/MainActivity.kt
+++ Tracer/app/src/main/java/com/korea50k/tracer/MainActivity.kt
@@ -1,54 +1,68 @@
 package com.korea50k.tracer
 
+import android.content.DialogInterface
 import android.content.Intent
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-
-import android.view.MenuItem;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.korea50k.tracer.Profile.ProfileFragment
+import com.korea50k.tracer.Ranking.RankingFragment
+import com.korea50k.tracer.Start.StartFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
+class MainActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity() , BottomNavigationView.OnNavigationItemSelectedListener {
+    //bottomNavigation 아이템 선택 리스너
+    private val navListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        var selectedFragment: Fragment? = null//선택된 프래그먼트 저장하는 변수
+
+        when (item.itemId) { //선택된 메뉴에 따라서 선택된 프래그 먼트 설정
+            R.id.navigation_start -> selectedFragment = StartFragment()
+            R.id.navigation_profile -> selectedFragment = ProfileFragment()
+            R.id.navigation_ranking -> selectedFragment = RankingFragment()
+        }
+
+        //동적으로 프래그먼트 교체
+        supportFragmentManager.beginTransaction().replace(
+            R.id.container,
+            selectedFragment!!
+        ).commit()
+
+        true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 곡률을 바꿔서 커스터마이징한 바텀 네비게이션 뷰 등록
-        val mView = customBottomBar as CurvedBottomNavigationView
-        mView.inflateMenu(R.menu.bottom_menu)
-        mView.setSelectedItemId(R.id.action_schedules)
-        //getting bottom navigation view and attaching the listener
-        mView.setOnNavigationItemSelectedListener(this@MainActivity)
-    }
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        val imageView = mainStartImageView
-        // 바텀 네비게이션뷰에서 메뉴 클릭했을 때,
-        //TODO:해당 프래그먼트 달아주기
-        when (item.itemId) {
-            R.id.action_favorites -> {
-                imageView.visibility = View.GONE
-            }
-            R.id.action_schedules -> {
-                imageView.visibility = View.VISIBLE
-            }
-            R.id.action_music -> {
-                imageView.visibility = View.GONE
-            }
+        //선택한 메뉴로 프래그먼트 바꿈
+        bottom_navigation.setOnNavigationItemSelectedListener(navListener)
+        //회전됐을 때 프래그먼트 유지
+        //처음 실행 했을때 초기 프래그먼트 설정
+        if (savedInstanceState == null) {
+            bottom_navigation.selectedItemId = R.id.navigation_start
+            supportFragmentManager.beginTransaction().replace(
+                R.id.container,
+                StartFragment()
+            ).commit()
         }
-        return true
     }
+
     fun onClick(v: View) {
         when (v.id) {
+            /*
             R.id.mainTest -> {
-                val newIntent = Intent(this, RunningSaveActivity::class.java)
-                startActivity(newIntent)
+                    val newIntent = Intent(this, RunningSaveActivity::class.java)
+                    startActivity(newIntent)
+
             }
-            R.id.mainStartImageView -> {
-                //TODO:MAP START
-            }
+
+             */
         }
+
     }
 }
