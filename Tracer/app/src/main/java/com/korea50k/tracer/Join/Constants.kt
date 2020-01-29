b1ef4af801f6b15f38c3fe696f5d6608606a0c7a
Wonseyoung <seyoung574458@gmail.com>
2020-01-29 20:19:43 +0900

    로그인, 회원가입 구현

---
+++ Tracer/app/src/main/java/com/korea50k/tracer/Join/Constants.kt
@@ -0,0 +1,15 @@
+package com.korea50k.tracer.Join
+
+class Constants {
+    companion object {
+        val NICKNAME_RULS = "^(?=.*\\W).{3,12}\$"
+        //이메일 정규식
+        val EMAIL_RULS = "^[a-z0-9_+.-]+@([a-z0-9-]+\\.)+[a-z0-9]{2,4}\$"
+
+        // 성별 제한
+        val GENDER_RULS = "^(?=.*[a-zA-Z]).{0,10}\$"
+
+        // 나이 제한
+        val AGE_RULS = "^(?=.*\\d).{1,2}\$"
+    }
+}
