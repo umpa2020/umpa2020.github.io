b5083a31745d37b17435d60b727ae9228e7f2b6e
jungsoojin <jdyun53658315@gmail.com>
2020-02-03 07:58:54 +0900

    프로필 =

---
+++ Tracer/app/src/main/java/com/korea50k/tracer/profile/ProfileUserHistoryFragment.kt
@@ -0,0 +1,25 @@
+package com.korea50k.tracer.profile
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.korea50k.tracer.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ProfileUserHistoryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile_user_history, container, false)
+    }
+
+
+}
