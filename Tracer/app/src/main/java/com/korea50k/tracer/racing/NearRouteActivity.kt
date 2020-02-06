d252e8b46ee40305e60cc82176f1546a7db23247
jungsoojin <jdyun53658315@gmail.com>
2020-02-06 14:20:09 +0900

    racing 버튼 누를때 현재 위치 인텐트로 넘김

--- Tracer/app/src/main/java/com/korea50k/tracer/racing/NearRouteActivity.kt
+++ Tracer/app/src/main/java/com/korea50k/tracer/racing/NearRouteActivity.kt
@@ -1,14 +1,18 @@
 package com.korea50k.tracer.racing
 
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.maps.model.LatLng
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.maps.android.SphericalUtil
 import com.korea50k.tracer.R
 import com.korea50k.tracer.dataClass.NearMap
+import com.korea50k.tracer.dataClass.RouteData
 import kotlinx.android.synthetic.main.activity_near_route.*
 import kotlinx.android.synthetic.main.recycler_nearactivity_item.*
 
@@ -103,9 +107,29 @@ class NearRouteActivity : AppCompatActivity() {
 
 
      */
+    var cur_loc = LatLng(0.0,0.0)          //현재위치
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_near_route)
+
+        //startFragment에서 넘긴 현재 위치를 intent로 받음
+        val intent = getIntent()
+        var current =intent.extras?.getParcelable<Location>("currentLocation")
+
+        Log.d("jsj", "near 넘어와서 getintent 한 값 " + current.toString())
+
+        //결과로 가져온 location에서 정보추출 / 이건 위도 경도 형태로 받아오는 형식
+        //Location 형태로 받아오고 싶다면 아래처럼
+        //var getintentLocation = current
+        var lat = current!!.latitude
+        var lng = current!!.longitude
+        cur_loc = LatLng(lat, lng)
+
+        Log.d("jsj", lat.toString() + lng.toString())
+
+
+
 
         //TODO. 서버에서 데이터 받아옴
         val datas = ArrayList<NearMap>()
@@ -121,5 +145,8 @@ class NearRouteActivity : AppCompatActivity() {
 
         near_recycler_map.adapter = NearRecyclerViewAdapter(datas)
         near_recycler_map.layoutManager = LinearLayoutManager(this)
+
+
+
     }
 }
