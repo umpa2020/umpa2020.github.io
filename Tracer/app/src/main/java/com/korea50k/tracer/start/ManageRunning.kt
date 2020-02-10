cd7ecd6496611d09b6d0d68e940d49ba8b745c8d
jungsoojin <jdyun53658315@gmail.com>
2020-02-10 16:21:08 +0900

    ManaageRunning 주석 해제

--- Tracer/app/src/main/java/com/korea50k/tracer/start/ManageRunning.kt
+++ Tracer/app/src/main/java/com/korea50k/tracer/start/ManageRunning.kt
@@ -36,37 +36,37 @@ class ManageRunning {
     var i = 0
     fun startRunning(activity:RunningActivity) {
         runningMap.startTracking()
-//
-//        Log.d(WSY, "거리 : " + runningMap.distance.toString())
-//        distanceThread = Thread(Runnable { // 거리 계산해주는 스레드 -> 일시 정지, 정지 해도 스레드 계속 실행됨.
-//            try {
-//                while(true) {
-//                    Thread.sleep(1000)
-//                    Log.d(WSY,"???")
-//                    Log.d(WSY, "거리 : " + runningMap.distance.toString())
-//                    activity.runOnUiThread(Runnable {
-//                        activity.runningDistanceTextView.text=String.format("%.3f",(runningMap.distance/1000))
-//                        i += 1
-//                        Log.d(WSY,  i.toString())
-//                    })
-//                }
-//            }catch (e : InterruptedException){
-//                Log.d(WSY,"intertupt() 실행")
-//            }
-//
-//        })
-//        distanceThread.start()
-//
-//        chronometer=activity.runningTimerTextView
-//        chronometer.base=SystemClock.elapsedRealtime()
-//        chronometer.start()
+
+        Log.d(WSY, "거리 : " + runningMap.distance.toString())
+        distanceThread = Thread(Runnable { // 거리 계산해주는 스레드 -> 일시 정지, 정지 해도 스레드 계속 실행됨.
+            try {
+                while(true) {
+                    Thread.sleep(1000)
+                    Log.d(WSY,"???")
+                    Log.d(WSY, "거리 : " + runningMap.distance.toString())
+                    activity.runOnUiThread(Runnable {
+                        activity.runningDistanceTextView.text=String.format("%.3f",(runningMap.distance/1000))
+                        i += 1
+                        Log.d(WSY,  i.toString())
+                    })
+                }
+            }catch (e : InterruptedException){
+                Log.d(WSY,"intertupt() 실행")
+            }
+
+        })
+        distanceThread.start()
+
+        chronometer=activity.runningTimerTextView
+        chronometer.base=SystemClock.elapsedRealtime()
+        chronometer.start()
     }
     fun restartRunning() {
         runningMap.restartTracking()
-//
-//        chronometer.base=SystemClock.elapsedRealtime()+timeWhenStopped
-//        chronometer.start()
-      //  distanceThread.start()
+
+        chronometer.base=SystemClock.elapsedRealtime()+timeWhenStopped
+        chronometer.start()
+        distanceThread.start()
     }
 
     fun pauseRunning() {
@@ -75,34 +75,34 @@ class ManageRunning {
 
 
 
-//        timeWhenStopped=chronometer.base-SystemClock.elapsedRealtime()
-//        chronometer.stop()
-//        activity.pause()
-//        privacy=Privacy.PUBLIC
-//
+        timeWhenStopped=chronometer.base-SystemClock.elapsedRealtime()
+        chronometer.stop()
+        activity.pause()
+        privacy=Privacy.PUBLIC
+
     }
 
     fun stopRunning() {
-//        val formatter = SimpleDateFormat("mm:ss", Locale.KOREA)
-//        formatter.setTimeZone(TimeZone.getTimeZone("UTC"))
-//        var routeData = RouteData()
-//        var infoData = InfoData()
-//
-//        //TODO: 이거 RX 기반인거 같아서 주석
-//        //activity.runningHandle.clicks()
-//        runningMap.stopTracking(routeData, infoData)
-//
-//        infoData.distance = runningMap.distance
-//        infoData.time = SystemClock.elapsedRealtime() - chronometer.base
-//        infoData.privacy=privacy
-//
-//        var newIntent = Intent((context as Activity), RunningSaveActivity::class.java)
-//        newIntent.putExtra("Route Data", routeData)
-//        newIntent.putExtra("Info Data", infoData)
-//        context.startActivity(newIntent)
-//        activity.finish()
-//
-//        distanceThread.interrupt() // 일시 정지 상태의 스레드에서 InterruptedException 예외를 발생시켜, 예외 처리 코드(catch)에서 실행 대기 상태로 가거나 종료 상태로 갈 수 있도록 한다.
+        val formatter = SimpleDateFormat("mm:ss", Locale.KOREA)
+        formatter.setTimeZone(TimeZone.getTimeZone("UTC"))
+        var routeData = RouteData()
+        var infoData = InfoData()
+
+        //TODO: 이거 RX 기반인거 같아서 주석
+        //activity.runningHandle.clicks()
+        runningMap.stopTracking(routeData, infoData)
+
+        infoData.distance = runningMap.distance
+        infoData.time = SystemClock.elapsedRealtime() - chronometer.base
+        infoData.privacy=privacy
+
+        var newIntent = Intent((context as Activity), RunningSaveActivity::class.java)
+        newIntent.putExtra("Route Data", routeData)
+        newIntent.putExtra("Info Data", infoData)
+        context.startActivity(newIntent)
+        activity.finish()
+
+        distanceThread.interrupt() // 일시 정지 상태의 스레드에서 InterruptedException 예외를 발생시켜, 예외 처리 코드(catch)에서 실행 대기 상태로 가거나 종료 상태로 갈 수 있도록 한다.
 
     }
 }
