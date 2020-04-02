1cd4abccbacf2d1164177c670460b0ec4e670638
신윤권 <syg4806@gmail.com>
2020-04-02 22:26:40 +0900

    ??

--- Tracer/app/src/main/java/com/umpa2020/tracer/main/start/running/RunningActivity.kt
+++ Tracer/app/src/main/java/com/umpa2020/tracer/main/start/running/RunningActivity.kt
@@ -76,6 +76,8 @@ class RunningActivity : AppCompatActivity(), OnDrawerScrollListener, OnDrawerOpe
     requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
     setContentView(R.layout.activity_running)
 
+
+    
     supportActionBar?.title = "RUNNING"
 
     init()
