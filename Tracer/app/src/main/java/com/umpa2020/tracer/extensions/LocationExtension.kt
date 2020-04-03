1118baa1655b4464caae78457197dedce5d9cdd7
신윤권 <syg4806@gmail.com>
2020-04-03 14:51:51 +0900

    change startFragment
    local broadcast & make new tracemap

--- Tracer/app/src/main/java/com/umpa2020/tracer/extensions/LocationExtension.kt
+++ Tracer/app/src/main/java/com/umpa2020/tracer/extensions/LocationExtension.kt
@@ -1,8 +1,12 @@
 package com.umpa2020.tracer.extensions
 
+import android.location.Location
 import com.google.android.gms.maps.model.LatLng
 import io.jenetics.jpx.WayPoint
 
+fun Location.toLatLng():LatLng{
+  return LatLng(latitude.toDouble(), longitude.toDouble())
+}
 fun WayPoint.toLatLng(): LatLng {
   return LatLng(latitude.toDouble(), longitude.toDouble())
 }
