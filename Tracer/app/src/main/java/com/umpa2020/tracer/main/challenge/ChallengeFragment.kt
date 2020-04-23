13381b17216dbf5e12b21883ba609ec638962818
Teddy2212 <39071803+Teddy2212@users.noreply.github.com>
2020-04-23 11:31:24 +0900

    쓰레기값 넣기

--- Tracer/app/src/main/java/com/umpa2020/tracer/main/challenge/ChallengeFragment.kt
+++ Tracer/app/src/main/java/com/umpa2020/tracer/main/challenge/ChallengeFragment.kt
@@ -5,7 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.umpa2020.tracer.R
+import com.umpa2020.tracer.dataClass.ChallengeData
+import com.umpa2020.tracer.main.challenge.ChallengeRecyclerViewAdapter
+import kotlinx.android.synthetic.main.fragment_challenge.*
 
 class ChallengeFragment : Fragment() {
 
@@ -14,6 +19,20 @@ class ChallengeFragment : Fragment() {
   ): View? {
     // Inflate the layout for this fragment
     val view: View = inflater.inflate(R.layout.fragment_challenge, container, false)
+    val challengeDatas = arrayListOf<ChallengeData>()
+
+    var challengeData = ChallengeData(0," "," "," ")
+    challengeDatas.add(challengeData)
+    challengeData = ChallengeData(0," "," "," ")
+    challengeDatas.add(challengeData)
+    challengeData = ChallengeData(0," "," "," ")
+    challengeDatas.add(challengeData)
+    challengeData = ChallengeData(0," "," "," ")
+    challengeDatas.add(challengeData)
+
+
+    challenge_recycler_view.adapter = ChallengeRecyclerViewAdapter(challengeDatas)
+    challenge_recycler_view.manager = ChallengeRecyclerViewAdapter.manager = LinearLayoutManger(this)
     return view
   }
 
