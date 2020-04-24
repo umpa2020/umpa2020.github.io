a027ab3c397ff20077479859b3755f6f95fc67ad
신윤권 <syg4806@gmail.com>
2020-04-24 22:56:52 +0900

    add numbering on sliding banner

--- Tracer/app/src/main/java/com/umpa2020/tracer/main/challenge/ChallengeFragment.kt
+++ Tracer/app/src/main/java/com/umpa2020/tracer/main/challenge/ChallengeFragment.kt
@@ -4,23 +4,39 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.size
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.umpa2020.tracer.R
+import com.umpa2020.tracer.dataClass.AdChallengeData
 import com.umpa2020.tracer.dataClass.ChallengeData
-import com.umpa2020.tracer.main.challenge.ChallengeRecyclerViewAdapter
+import com.umpa2020.tracer.util.Logg
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_challenge.*
 import kotlinx.android.synthetic.main.fragment_challenge.view.*
 
 class ChallengeFragment : Fragment() {
 
+  var adChallengeList = ArrayList<AdChallengeData>()
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
   ): View? {
     // Inflate the layout for this fragment
-
     val view: View = inflater.inflate(R.layout.fragment_challenge, container, false)
-    val challengeDatas = arrayListOf<ChallengeData>()
 
+    adChallengeList.add(AdChallengeData("test", R.drawable.ic_checkpoint_red))
+    adChallengeList.add(AdChallengeData("test2", R.drawable.ic_racing_finishpoint))
+    adChallengeList.add(AdChallengeData("test2", R.drawable.ic_checkpoint_gray))
+    adChallengeList.add(AdChallengeData("test2", R.drawable.ic_racing_startpoint))
+    view.adChallengeScrollViewPager.adapter = AdChallengePageAdapter(adChallengeList, requireContext())
+    view.adChallengeScrollViewPager.startAutoScroll()
+    //view.adChallengeCountTextView.text="${view.adChallengeScrollViewPager.currentItem}/${adChallengeList.size}"
+    val challengeDatas = arrayListOf<ChallengeData>()
+    val a=view.adChallengeScrollViewPager.currentItem
+    view.adChallengeScrollViewPager.addOnLayoutChangeListener { v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
+      adChallengeCountTextView.text="${(view.adChallengeScrollViewPager.currentItem-a)%adChallengeList.size+1}/${adChallengeList.size}"
+    }
     var challengeData = ChallengeData(R.drawable.button_background,"제주 그란폰도","2019. 04. 01","제주")
     challengeDatas.add(challengeData)
     challengeData = ChallengeData(R.drawable.button_background,"철원 DMZ 랠리","2019. 07. 03","강원")
@@ -45,10 +61,6 @@ class ChallengeFragment : Fragment() {
     view.btn_challenge_search.setOnClickListener{
 
     }
-
-
-
-
     view.challenge_recycler_view.adapter = ChallengeRecyclerViewAdapter(challengeDatas)
     view.challenge_recycler_view.layoutManager = GridLayoutManager(context, 2)
     return view
