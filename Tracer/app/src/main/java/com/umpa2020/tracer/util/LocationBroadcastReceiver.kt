36729c3bbcb73bc0887591044b26f9438ac17325
Wonseyoung <seyoung574458@gmail.com>
2020-04-25 00:08:36 +0900

    GPS 튐 방지
    GPS 튐 방지

--- Tracer/app/src/main/java/com/umpa2020/tracer/util/LocationBroadcastReceiver.kt
+++ Tracer/app/src/main/java/com/umpa2020/tracer/util/LocationBroadcastReceiver.kt
@@ -4,6 +4,9 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.location.Location
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.SphericalUtil
+import com.umpa2020.tracer.extensions.toLatLng
 import com.umpa2020.tracer.main.start.BaseRunningActivity
 
 /**
@@ -11,9 +14,27 @@ import com.umpa2020.tracer.main.start.BaseRunningActivity
  *  serevice로부터 신호(GPS)를 받아 인자값으로 들어오는 map에 전달.
  */
 class LocationBroadcastReceiver( val activity: BaseRunningActivity) : BroadcastReceiver() {
+    var previousLatLng = LatLng(0.0, 0.0)          //이전위
+    var currentLatLng = LatLng(0.0, 0.0)
+    var flag = true
     override fun onReceive(context: Context?, intent: Intent?) {
         val message = intent?.getParcelableExtra<Location>("message")
         val currentLocation = message as Location
-        activity.updateLocation(currentLocation)
+        currentLatLng = currentLocation.toLatLng()
+
+        if(flag){ // 맨 처음엔 이전 위치가 없으므로
+            Logg.d("맨 처음 위치 업데이트")
+            activity.updateLocation(currentLocation)
+            previousLatLng = currentLatLng
+            flag = false
+        }else {
+            if (SphericalUtil.computeDistanceBetween(previousLatLng, currentLatLng) < 20) {
+                activity.updateLocation(currentLocation)
+                previousLatLng = currentLatLng
+                Logg.d("GPS 예상 안")
+            }else{
+                Logg.d("GPS 튐")
+            }
+        }
     }
 }
