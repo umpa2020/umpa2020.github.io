8448edd2b873b7f28a0cd35e08c8efd8cfb16d0f
Wonseyoung <seyoung574458@gmail.com>
2020-04-25 00:25:22 +0900

    시간 최소 값 수정
    시간 최소 값 수정

--- Tracer/app/src/main/java/com/umpa2020/tracer/util/LocationBroadcastReceiver.kt
+++ Tracer/app/src/main/java/com/umpa2020/tracer/util/LocationBroadcastReceiver.kt
@@ -14,36 +14,38 @@ import com.umpa2020.tracer.main.start.BaseRunningActivity
  *  브로드 케스트 리시버 클래스
  *  serevice로부터 신호(GPS)를 받아 인자값으로 들어오는 map에 전달.
  */
-class LocationBroadcastReceiver( val activity: BaseRunningActivity) : BroadcastReceiver() {
-    var previousLatLng = LatLng(0.0, 0.0)          //이전위
-    var currentLatLng = LatLng(0.0, 0.0)
+class LocationBroadcastReceiver(val activity: BaseRunningActivity) : BroadcastReceiver() {
+  var previousLatLng = LatLng(0.0, 0.0)          //이전위
+  var currentLatLng = LatLng(0.0, 0.0)
 
-    var previousTime = 0L
-    var currentTime = 0L
+  var previousTime = 0L
+  var currentTime = 0L
 
-    var flag = true
+  var flag = true
 
-    override fun onReceive(context: Context?, intent: Intent?) {
-        val message = intent?.getParcelableExtra<Location>("message")
-        val currentLocation = message as Location
-        currentLatLng = currentLocation.toLatLng()
-        currentTime = SystemClock.elapsedRealtime()
+  override fun onReceive(context: Context?, intent: Intent?) {
+    val message = intent?.getParcelableExtra<Location>("message")
+    val currentLocation = message as Location
+    currentLatLng = currentLocation.toLatLng()
+    currentTime = SystemClock.elapsedRealtime()
 
-        if(flag){ // 맨 처음엔 이전 위치가 없으므로
-            Logg.d("맨 처음 위치 업데이트")
-            activity.updateLocation(currentLocation)
-            previousLatLng = currentLatLng
-            previousTime = currentTime
-            flag = false
-        }else {
-            if ((currentTime - previousTime)/1000 * 10 > SphericalUtil.computeDistanceBetween(previousLatLng, currentLatLng)) {
-                activity.updateLocation(currentLocation)
-                previousLatLng = currentLatLng
-                previousTime = currentTime
-                Logg.d("GPS 예상 안")
-            }else{
-                Logg.d("GPS 튐")
-            }
-        }
+    if (flag) { // 맨 처음엔 이전 위치가 없으므로
+      Logg.d("맨 처음 위치 업데이트")
+      activity.updateLocation(currentLocation)
+      previousLatLng = currentLatLng
+      previousTime = currentTime
+      flag = false
+    } else {
+      if ((((currentTime - previousTime) / 1000) + 1) * 10
+          > SphericalUtil.computeDistanceBetween(previousLatLng, currentLatLng)
+      ) {
+        activity.updateLocation(currentLocation)
+        previousLatLng = currentLatLng
+        previousTime = currentTime
+        Logg.d("GPS 예상 안")
+      } else {
+        Logg.d("GPS 튐")
+      }
     }
+  }
 }
