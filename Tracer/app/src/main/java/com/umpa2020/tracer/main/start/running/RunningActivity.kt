71c11be87045660eea03ef797dfd4d93fc04f9b9
Wonseyoung <seyoung574458@gmail.com>
2020-05-15 18:50:45 +0900

    러닝 액티비티 import 충돌 해결

--- Tracer/app/src/main/java/com/umpa2020/tracer/main/start/running/RunningActivity.kt
+++ Tracer/app/src/main/java/com/umpa2020/tracer/main/start/running/RunningActivity.kt
@@ -14,20 +14,10 @@ import com.umpa2020.tracer.constant.Privacy
 import com.umpa2020.tracer.constant.UserState
 import com.umpa2020.tracer.dataClass.InfoData
 import com.umpa2020.tracer.dataClass.RouteGPX
-<<<<<<< HEAD
-import com.umpa2020.tracer.extensions.makingIcon
 import com.umpa2020.tracer.extensions.toWayPoint
 import com.umpa2020.tracer.gpx.WayPointType.*
 import com.umpa2020.tracer.main.start.BaseRunningActivity
 import com.umpa2020.tracer.util.ChoicePopup
-import com.umpa2020.tracer.util.Logg
-=======
-import com.umpa2020.tracer.extensions.toWayPoint
-import com.umpa2020.tracer.gpx.WayPointType.*
-import com.umpa2020.tracer.main.start.BaseRunningActivity
-import com.umpa2020.tracer.roomDatabase.viewModel.RecordViewModel
-import com.umpa2020.tracer.util.ChoicePopup
->>>>>>> master
 import com.umpa2020.tracer.util.TTS
 import kotlinx.android.synthetic.main.activity_running.*
 
