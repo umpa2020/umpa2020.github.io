a83d5fd5137139e1426ab838796f1f4312c6a38e
JeongHyunKim <teddy221284@gmail.com>
2020-05-18 13:31:51 +0900

    업적 아이템 생성

--- Tracer/app/src/main/java/com/umpa2020/tracer/main/profile/myachievement/ProfileAchievementActivity.kt
+++ Tracer/app/src/main/java/com/umpa2020/tracer/main/profile/myachievement/ProfileAchievementActivity.kt
@@ -2,7 +2,13 @@ package com.umpa2020.tracer.main.profile.myachievement
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import com.umpa2020.tracer.R
+import com.umpa2020.tracer.dataClass.AchievementData
+import com.umpa2020.tracer.dataClass.ChallengeData
+import com.umpa2020.tracer.main.challenge.ChallengeRecyclerViewAdapter
+import kotlinx.android.synthetic.main.activity_profile_achivement.*
+import kotlinx.android.synthetic.main.fragment_challenge.*
 
 /**
  * 나의 업적 액티비티
@@ -14,5 +20,9 @@ class ProfileAchievementActivity : AppCompatActivity() {
     setContentView(R.layout.activity_profile_achivement)
 
 
+  }
+
+  private fun achievementDataList(listAchievementData: MutableList<AchievementData>) {
+//    profileRecyclerAchievement.layoutManager = GridLayoutManager(context, 3)
   }
 }
